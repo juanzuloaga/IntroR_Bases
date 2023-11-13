0d6383008972cb868881eea98544e1ba88bd8dbd
juanzuloaga <jzuloaga65@gmail.com>
2023-11-13 12:41:50 -0700

    Revisión por Juan

--- to_run_Bases.R
+++ to_run_Bases.R
@@ -1,6 +1,6 @@
 
 
-setwd("C:/Talleres_Ecodiversa_R/")
+setwd("C:/Talleres_R/")
 rmarkdown::render(input = "./IntroR_Bases/IntroR_Bases.Rmd",
                   output_file =  "IntroR_Bases.html",
                   output_dir = "./IntroR_Bases")
